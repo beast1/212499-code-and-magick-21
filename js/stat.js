a1bff739ec7014461c2cf621411038c1b828937e
Егор Глебов <beast.loby@gmail.com>
2020-09-21 18:22:59 +0300

    stat.js fix according review

--- js/stat.js
+++ js/stat.js
@@ -1,31 +1,21 @@
 'use strict';
 
 const Cloud = {
-  position: {
-    X: 110,
-    Y: 10
-  },
-  size: {
-    WIDTH: 420,
-    HEIGHT: 270,
-    PADDING: 25,
-    NEW_LINE: 20
-  }
+  X: 110,
+  Y: 10,
+  WIDTH: 420,
+  HEIGHT: 270,
+  PADDING: 25,
+  NEW_LINE: 20
 };
 
 const Bar = {
-  size: {
-    MAX_HEIGHT: 150,
-    WIDTH: 40,
-    GAP: 50
-  },
-  fill: {
-    ACCENT: `rgba(255, 0, 0, 1)`
-  },
-  value: {
-    HEIGHT: 30,
-    PADDING: 10
-  }
+  MAX_HEIGHT: 150,
+  WIDTH: 40,
+  GAP: 50,
+  FILL: `rgba(255, 0, 0, 1)`,
+  VALUE_HEIGHT: 30,
+  VALUE_PADDING: 10
 };
 
 const HEX = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `A`, `B`, `C`, `D`, `E`, `F`];
@@ -41,20 +31,20 @@ function randomHEXNum() {
 
 function renderCloud(ctx, cloudX, cloudY, cloudFill) {
   ctx.fillStyle = cloudFill;
-  ctx.fillRect(cloudX, cloudY, Cloud.size.WIDTH, Cloud.size.HEIGHT);
+  ctx.fillRect(cloudX, cloudY, Cloud.WIDTH, Cloud.HEIGHT);
 }
 
 function renderBar(ctx, barX, barY, barFill, barValue, maxValue) {
-  const BAR_HEIGHT = (barValue * Bar.size.MAX_HEIGHT) / maxValue;
+  const BAR_HEIGHT = (barValue * Bar.MAX_HEIGHT) / maxValue;
   ctx.fillStyle = barFill;
-  ctx.fillRect(barX, barY + Bar.size.MAX_HEIGHT - BAR_HEIGHT, Bar.size.WIDTH, BAR_HEIGHT);
+  ctx.fillRect(barX, barY + Bar.MAX_HEIGHT - BAR_HEIGHT, Bar.WIDTH, BAR_HEIGHT);
   ctx.fillStyle = `black`;
-  ctx.fillText((barValue).toFixed(), barX, Cloud.size.HEIGHT - Bar.value.HEIGHT - BAR_HEIGHT - Bar.value.PADDING);
+  ctx.fillText((barValue).toFixed(), barX, Cloud.HEIGHT - Bar.VALUE_HEIGHT - BAR_HEIGHT - Bar.VALUE_PADDING);
 }
 // s
 function renderBars(ctx, names, times) {
   const bars = [];
-  let barX = Cloud.position.X + Cloud.size.PADDING * 1.5;
+  let barX = Cloud.X + Cloud.PADDING * 1.5;
   let maxTimes = times[0];
   const pushBarData = (name, time, fill) => {
     bars.push({
@@ -70,7 +60,7 @@ function renderBars(ctx, names, times) {
     }
 
     if (names[i] === `Вы`) {
-      pushBarData(names[i], times[i], Bar.fill.ACCENT);
+      pushBarData(names[i], times[i], Bar.FILL);
     }
   }
 
@@ -82,19 +72,19 @@ function renderBars(ctx, names, times) {
 
   bars.forEach(function (bar) {
     ctx.fillStyle = `black`;
-    ctx.fillText(bar.name, barX, Cloud.size.HEIGHT - Bar.value.PADDING);
-    renderBar(ctx, barX, Cloud.position.Y + Cloud.size.PADDING + (Cloud.size.NEW_LINE * 3 - 5), bar.fill, bar.time, maxTimes);
-    barX = barX + Bar.size.WIDTH + Bar.size.GAP;
+    ctx.fillText(bar.name, barX, Cloud.HEIGHT - Bar.VALUE_PADDING);
+    renderBar(ctx, barX, Cloud.Y + Cloud.PADDING + (Cloud.NEW_LINE * 3 - 5), bar.fill, bar.time, maxTimes);
+    barX = barX + Bar.WIDTH + Bar.GAP;
   });
 }
 
-function renderStatistics(ctx, names, times) {
-  renderCloud(ctx, Cloud.position.X + 10, Cloud.position.Y + 10, `rgb(0, 0, 0, .7)`);
-  renderCloud(ctx, Cloud.position.X, Cloud.position.Y, `white`);
+window.renderStatistics = (ctx, names, times) => {
+  renderCloud(ctx, Cloud.X + 10, Cloud.Y + 10, `rgb(0, 0, 0, .7)`);
+  renderCloud(ctx, Cloud.X, Cloud.Y, `white`);
 
   ctx.fillStyle = `black`;
-  ctx.fillText(`Ура вы победили!`, Cloud.position.X + Cloud.size.PADDING, Cloud.position.Y + Cloud.size.PADDING);
-  ctx.fillText(`Список результатов: `, Cloud.position.X + Cloud.size.PADDING, Cloud.position.Y + Cloud.size.PADDING + Cloud.size.NEW_LINE);
+  ctx.fillText(`Ура вы победили!`, Cloud.X + Cloud.PADDING, Cloud.Y + Cloud.PADDING);
+  ctx.fillText(`Список результатов: `, Cloud.X + Cloud.PADDING, Cloud.Y + Cloud.PADDING + Cloud.NEW_LINE);
 
   renderBars(ctx, names, times);
-}
+};
